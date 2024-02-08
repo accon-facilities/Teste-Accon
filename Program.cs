6ba5ab4a0b3c4c9bff964fea464e30c574380585
Alvaro Galhardo <alvaro.fernandes@accon.tech>
2024-02-08 11:17:00 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Teste_Accon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
